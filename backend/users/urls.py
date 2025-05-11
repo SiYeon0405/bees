f6c5337923e99ec9a534d2507a20b493574614c1
SiYeon0405 <parksy03080916@gmail.com>
2025-05-11 21:23:34 +0900

    feat: 회원가입 기능 구현 및 DB 마이그레이션 반영

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 # users/urls.py
-=======
->>>>>>> b8481a2 (feat: 회원가입 기능 복구 및 정상 작동 확인)
 from django.urls import path
 from .views import SignupView
 
 urlpatterns = [
     path('signup/', SignupView.as_view()),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> b8481a2 (feat: 회원가입 기능 복구 및 정상 작동 확인)
