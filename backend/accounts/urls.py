d56061a8091323bbf9ccbb5303e8135ab8c087a1
youmintee <85918429+nomaea@users.noreply.github.com>
2025-06-04 12:58:04 +0900

    Delete backend/accounts directory

--- backend/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.github_login_view, name='github_login'),
-    path('github-success/', views.github_success, name='github_success'),
-]
